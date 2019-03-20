24623400d5997b118643f14ed21007ac0d4d2fc1
Tatsuya Fukuta <fukuta_tatsuya@intec.co.jp>
2019-03-20 14:27:50 +0900

    Updates

---
+++ admin/0.76dd9f0ade4fdde3e0e8.hot-update.js
@@ -0,0 +1,173 @@
+webpackHotUpdate(0,{
+
+/***/ 44:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(9);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _Content__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(45);
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(23);
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var any_promise__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(191);
+/* harmony import */ var any_promise__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(any_promise__WEBPACK_IMPORTED_MODULE_3__);
+var _jsxFileName = "/Users/fukuta/workspace/GitHub/fukuta-tatsuya-intec/homepage_en/src/components/AboutPageTemplate/index.js";
+
+(function () {
+  var enterModule = __webpack_require__(7).enterModule;
+
+  enterModule && enterModule(module);
+})();
+
+
+
+
+
+
+var AboutPageTemplate = function AboutPageTemplate(_ref) {
+  var title = _ref.title,
+      content = _ref.content,
+      image = _ref.image,
+      contentComponent = _ref.contentComponent;
+  var PageContent = contentComponent || _Content__WEBPACK_IMPORTED_MODULE_1__["default"];
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+    className: "hero is-primary is-bold is-medium",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "hero-body",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "columns",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "column is-10 is-offset-1",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "section",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    className: "title",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }, title))))))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+    className: "section section--gradient",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 26
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 27
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "columns",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 28
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "column is-10 is-offset-1",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 29
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "section",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 30
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    alt: "",
+    src: image,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 31
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(PageContent, {
+    className: "content",
+    content: content,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 32
+    },
+    __self: this
+  })))))));
+};
+
+AboutPageTemplate.propTypes = {
+  title: prop_types__WEBPACK_IMPORTED_MODULE_2___default.a.string.isRequired,
+  content: prop_types__WEBPACK_IMPORTED_MODULE_2___default.a.string,
+  image: prop_types__WEBPACK_IMPORTED_MODULE_2___default.a.string,
+  contentComponent: prop_types__WEBPACK_IMPORTED_MODULE_2___default.a.func
+};
+var _default = AboutPageTemplate;
+/* harmony default export */ __webpack_exports__["default"] = (_default);
+;
+
+(function () {
+  var reactHotLoader = __webpack_require__(7).default;
+
+  var leaveModule = __webpack_require__(7).leaveModule;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(AboutPageTemplate, "AboutPageTemplate", "/Users/fukuta/workspace/GitHub/fukuta-tatsuya-intec/homepage_en/src/components/AboutPageTemplate/index.js");
+  reactHotLoader.register(_default, "default", "/Users/fukuta/workspace/GitHub/fukuta-tatsuya-intec/homepage_en/src/components/AboutPageTemplate/index.js");
+  leaveModule(module);
+})();
+
+;
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(6)(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=0.76dd9f0ade4fdde3e0e8.hot-update.js.map
---
+++ admin/0.ca0bbeb5175179e911d3.hot-update.js
@@ -0,0 +1,11 @@
+webpackHotUpdate(0,{
+
+/***/ 64:
+/***/ (function(module) {
+
+module.exports = [{"componentChunkName":"component---src-templates-home-page-js","jsonName":"index","path":"/"},{"componentChunkName":"component---src-pages-404-js","jsonName":"404-html-516","path":"/404.html"},{"componentChunkName":"component---src-pages-404-js","jsonName":"404-22d","path":"/404/"},{"componentChunkName":"component---src-templates-about-page-js","jsonName":"about-f34","path":"/about/"},{"componentChunkName":"component---src-templates-blog-js","jsonName":"blog-c06","path":"/blog"},{"componentChunkName":"component---src-templates-article-page-js","jsonName":"blog-lorem-ipsum-dolor-situm-ea6","path":"/blog/lorem-ipsum-dolor-situm/"},{"componentChunkName":"component---src-templates-article-page-js","jsonName":"blog-welcome-to-rdbox-cf1","path":"/blog/welcome-to-rdbox/"},{"componentChunkName":"component---cache-dev-404-page-js","jsonName":"dev-404-page-5f9","path":"/dev-404-page/"},{"componentChunkName":"component---src-pages-tags-index-js","jsonName":"tags-275","path":"/tags/"},{"componentChunkName":"component---src-templates-tags-js","jsonName":"tags-ipsum-a5a","path":"/tags/ipsum/"},{"componentChunkName":"component---src-templates-tags-js","jsonName":"tags-lorem-f18","path":"/tags/lorem/"}];
+
+/***/ })
+
+})
+//# sourceMappingURL=0.ca0bbeb5175179e911d3.hot-update.js.map
